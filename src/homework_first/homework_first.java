461816752ff98c51ab2af3a489d3236297c8e5c2
sevenine-qq <627054777@qq.com>
2021-10-08 20:27:34 +0800

    2-4

--- src/homework_first/homework_first.java
+++ src/homework_first/homework_first.java
@@ -6,13 +6,12 @@ public class homework_first {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-       Scanner input = new Scanner(System.in);
-       System.out.print("Enter three numbers: ");
-       double number1 = input.nextDouble();
-       double number2 = input.nextDouble();
-       double number3 = input.nextDouble();
-       double average = (number1 + number2 + number3) / 3;
-       System.out.println("The average of " + number1 + " " + number2 + " " + number3 + " " + " is " + average);
+       final double PI=3.14159;
+       Scanner input=new Scanner(System.in);
+       System.out.print("Enter a number for radius: ");
+       double radius = input.nextDouble();
+       double area = radius * radius * PI;
+       System.out.println("The area for the circle of radius " + radius + " is " + area);
 	}
 
 }
