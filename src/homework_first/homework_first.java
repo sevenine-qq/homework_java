39927fcba9bf318aad55b559f605133f51b81b8e
sevenine-qq <627054777@qq.com>
2021-10-08 18:19:10 +0800

    1-6

--- src/homework_first/homework_first.java
+++ src/homework_first/homework_first.java
@@ -4,7 +4,8 @@ public class homework_first {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        System.out.println(1/0);
+        System.out.print("Celsius 35 is Fahrenheit degree");
+        System.out.println((9 / 5) * 35 + 32);
 	}
 
 }
