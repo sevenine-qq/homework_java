f247396f8eceb4d274cb09cc0d8bc4384ff16f60
sevenine-qq <627054777@qq.com>
2021-10-08 20:40:09 +0800

    2-5

--- src/homework_first/homework_first.java
+++ src/homework_first/homework_first.java
@@ -6,12 +6,14 @@ public class homework_first {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-       final double PI=3.14159;
-       Scanner input=new Scanner(System.in);
-       System.out.print("Enter a number for radius: ");
-       double radius = input.nextDouble();
-       double area = radius * radius * PI;
-       System.out.println("The area for the circle of radius " + radius + " is " + area);
+		final double PI=3.14159;
+	       Scanner input = new Scanner(System.in);
+	       System.out.print("Enter an integer for seconds: ");
+	       int seconds = input.nextInt();
+	       int minutes = seconds / 60;
+	       int remainingSeconds = seconds % 60;
+	       System.out.println(seconds + " seconds is " + minutes + " minutes and " + remainingSeconds + "seconds");
+       
 	}
 
 }
