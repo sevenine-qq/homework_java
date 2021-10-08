7309ce3ef68b485dca63d9a38328e67e2b5b529b
sevenine-qq <627054777@qq.com>
2021-10-08 21:06:05 +0800

    2-8

--- src/homework_first/homework_first.java
+++ src/homework_first/homework_first.java
@@ -6,15 +6,11 @@ public class homework_first {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		long totalMilliseconds = System.currentTimeMillis();
-		long totalSeconds = totalMilliseconds / 1000;
-		long currentSecond = totalSeconds % 60;
-		long totalMinutes = totalSeconds / 60;
-		long currentMinute = totalMinutes % 60;
-		long totalHours = totalMinutes / 60;
-		long currentHour = totalHours % 24;
-		System.out.println("Current time is " + currentHour + ":" + currentMinute + ":" + currentSecond + " GMT ");
-	      
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter purchase amount: ");
+		double purchaseAmount = input.nextDouble();
+		double tax = purchaseAmount * 0.06;
+		System.out.println("Sales tax is $" + (int)(tax * 100)/100.0);
        
 	}
 
