28752e46c5a4ba2a5be8400bf90c1d359af2260c
sevenine-qq <627054777@qq.com>
2021-10-08 16:36:24 +0800

    1-3

--- src/homework_first/homework_first.java
+++ src/homework_first/homework_first.java
@@ -4,9 +4,8 @@ public class homework_first {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        System.out.println("Programming is fun!");
-        System.out.println("Fundamentals First");
-        System.out.println("Problem Driven");
+        System.out.print("(10.5 + 2 * 3)/(45 - 3.5) = ");
+        System.out.println((10.5+2*3)/(45-3.5));
 	}
 
 }
