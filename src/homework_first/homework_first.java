cbff2a4cbf69559a123bc30f75a88998daa633ba
sevenine-qq <627054777@qq.com>
2021-10-08 19:27:14 +0800

    2-2

--- src/homework_first/homework_first.java
+++ src/homework_first/homework_first.java
@@ -1,13 +1,15 @@
 package homework_first;
 
+import java.util.Scanner;
+
 public class homework_first {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        double radius;
-        double area;
-        radius=20;
-        area=radius*radius*3.14159;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number for radius:");
+        double radius = input.nextDouble();
+        double area = radius * radius *3.14159;
         System.out.println("The area for the circle of radius " + radius + " is " + area);
 	}
 
