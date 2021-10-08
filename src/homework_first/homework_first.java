d4f1518f8d3e221c1d55af3a115b5e344c5f7ac9
sevenine-qq <627054777@qq.com>
2021-10-08 16:30:09 +0800

    homework_first_two

--- src/homework_first/homework_first.java
+++ src/homework_first/homework_first.java
@@ -4,8 +4,9 @@ public class homework_first {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-        System.out.println("Welcom to java!");
+        System.out.println("Programming is fun!");
+        System.out.println("Fundamentals First");
+        System.out.println("Problem Driven");
 	}
 
 }
