c2cbee5d2837db70c28f3f308620dd118eb75e84
sevenine-qq <627054777@qq.com>
2021-10-08 21:44:45 +0800

    2-9

--- src/homework_first/homework_first.java
+++ src/homework_first/homework_first.java
@@ -7,11 +7,22 @@ public class homework_first {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner input = new Scanner(System.in);
-		System.out.print("Enter purchase amount: ");
-		double purchaseAmount = input.nextDouble();
-		double tax = purchaseAmount * 0.06;
-		System.out.println("Sales tax is $" + (int)(tax * 100)/100.0);
-       
+		System.out.print("Enter annual interest rate, e.g., 7.25: ");
+		double annualInterestRate = input.nextDouble();
+		
+		double monthlyInterestRate = annualInterestRate / 1200;
+		
+		System.out.print("Enter number of years as an integer, e.g., 5:");
+		int numberOfYears = input.nextInt();
+		
+		System.out.print("Enter loan amount, e.g., 120000.95:");
+		double loanAmount = input.nextDouble();
+		
+		double monthlyPayment = loanAmount * monthlyInterestRate / (1 - 1/Math.pow(1 + monthlyInterestRate,numberOfYears * 12));
+		double totalPayment = monthlyPayment * numberOfYears * 12;
+		
+		System.out.println("The monthly payment is $" + (int)(monthlyPayment * 100) / 100.0);
+		System.out.println("The total payment is $" + (int)(totalPayment * 100) / 100.0);
 	}
 
 }
