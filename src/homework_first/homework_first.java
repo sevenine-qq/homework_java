1982f12fa1a33ddfc2b811c2bda6530e99f77323
sevenine-qq <627054777@qq.com>
2021-10-08 20:46:05 +0800

    2-6

--- src/homework_first/homework_first.java
+++ src/homework_first/homework_first.java
@@ -6,13 +6,12 @@ public class homework_first {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		final double PI=3.14159;
-	       Scanner input = new Scanner(System.in);
-	       System.out.print("Enter an integer for seconds: ");
-	       int seconds = input.nextInt();
-	       int minutes = seconds / 60;
-	       int remainingSeconds = seconds % 60;
-	       System.out.println(seconds + " seconds is " + minutes + " minutes and " + remainingSeconds + "seconds");
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter a degree in Fahrenheit: ");
+		double fahrenheit = input.nextDouble();
+		double celsius = (5.0/9) * (fahrenheit - 32);
+		System.out.println("Fahrenheit " + fahrenheit + " is " + celsius + " in Celsius");
+	      
        
 	}
 
