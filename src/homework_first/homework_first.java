1c501ac592bff7b44847f3a18078c7edea0302ba
sevenine-qq <627054777@qq.com>
2021-10-08 16:42:50 +0800

    1-4

--- src/homework_first/homework_first.java
+++ src/homework_first/homework_first.java
@@ -2,10 +2,9 @@ package homework_first;
 
 public class homework_first {
 
-	public static void main(String[] args) {
+	public static main(String[] args) {
 		// TODO Auto-generated method stub
-        System.out.print("(10.5 + 2 * 3)/(45 - 3.5) = ");
-        System.out.println((10.5+2*3)/(45-3.5));
+        System.out.println("Welcom to Java);
 	}
 
 }
