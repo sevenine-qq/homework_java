cd7a3a1b542b522412c77aec219e0a61af1b48f5
sevenine-qq <627054777@qq.com>
2021-12-23 17:42:02 +0800

    third

---
+++ src/homework_third/TotalArea.java
@@ -0,0 +1,28 @@
+package homework_third;
+
+public class TotalArea {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Circle[] circleArray;
+        
+        circleArray=createCircleArray();
+        printCircleArray(circleArray);
+	}
+	public static Circle[] createCircleArray() {
+		Circle[] circleArray=new Circle[5];
+		
+		for (int i=0;i<circleArray.length;i++) {
+			circleArray[i]=new Circle(Math.random()*100);
+		}
+		return circleArray;
+	}
+	public static void printCircleArray(Circle[] circleArray) {
+		System.out.printf("%-30s%-15s\n","Radius","Area");
+		for (int i=0;i<circleArray.length;i++) {
+			System.out.printf("%-30f%-50f\n",circleArray[i].getRadius(),circleArray[i].getArea());
+		}
+		
+	}
+
+}
